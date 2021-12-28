2080b4ee134823f07335f28fcb1ec32fbd88f864
xandre9-git <jmjames673@gmail.com>
2021-12-28 13:05:44 -0500

    Modified 'submitButton' to create new book entry, add to 'addLibraryBook' array, and display on page via 'displayBookOnPage()' function.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 function Books(title, author, pageNum, read) { // object constructor
-  this.title = title
-  this.author = author
-  this.pageNum = pageNum
-  this.read = read == 1 ? "Y":"N";  // 1 means read, 0 means not read yet
-  this.info = function info(string){
-   return string = `${this.title} by ${this.author}, ${this.pageNum} pages, read: ${this.read}`;
-  }
+	this.title = title;
+	this.author = author;
+	this.pageNum = pageNum;
+	this.read = read == 1 ? "Y" : "N"; // 1 means read, 0 means not read yet
+	this.info = function info(string) {
+		return string = `${this.title} by ${this.author}, ${this.pageNum} pages, read: ${this.read}`;
+	};
 }
 
 const rentalBook = new Books('The Book on Rental Property Investing: How to Create Wealth with Intelligent Buy and Hold Real Estate Investing', 'Brandon Turner', 356, 1);
 
-const cleanCode = new Books('Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin', 464, 0)
+const cleanCode = new Books('Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin', 464, 0);
 
 const eloquentJavaScript = new Books('Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming', 'Marijn Haverbeke', 472, 0);
 
@@ -23,7 +23,7 @@ const eloquentJavaScript = new Books('Eloquent JavaScript, 3rd Edition: A Modern
 let libraryBooks = [];
 
 function addLibraryBook(obj) {
-  libraryBooks.push(obj)
+	libraryBooks.push(obj);
 }
 
 // test books
@@ -37,128 +37,101 @@ addLibraryBook(rentalBook);
 
 const body = document.querySelector('body');
 const booksDiv = document.createElement('div');
-booksDiv.className = 'booksContainer'
+booksDiv.className = 'booksContainer';
 
-function bookIterator(arr, childNode){
-  let listItems = []
-  for (i = 0; i < arr.length; i++) {
-    console.log(arr[i].title)
-    console.log(document.querySelectorAll('.titleListItem')[0].textContent)
+function bookIterator(arr, childNode) {
 
-    // if item is already displayed on page don't run actions below
-    if (arr[i].title != document.querySelectorAll('.titleListItem')[0].textContent) {
-      const bookList = document.createElement('ul');
-      bookList.className = "bookList"
-      bookList.textContent = "";
-  
-      let titleListItem = document.createElement('li');
-      titleListItem.className = "titleListItem"
-      titleListItem.textContent = `${arr[i].title}`
-      listItems.push(titleListItem);
-  
-      let authorListItem = document.createElement('li');
-      authorListItem.className = "authorListItem"
-      authorListItem.textContent = `${arr[i].author}`
-      listItems.push(authorListItem);
-  
-      let pageNumListItem = document.createElement('li');
-      pageNumListItem.className = "pageNumListItem"
-      pageNumListItem.textContent = `${arr[i].pageNum}`
-      listItems.push(pageNumListItem);
-  
-      let readListItem = document.createElement('li');
-      readListItem.className = "readListItem"
-      readListItem.textContent = `${arr[i].read}`
-      listItems.push(readListItem);
-  
-      booksDiv.appendChild(bookList);
-      bookList.appendChild(titleListItem);
-      bookList.appendChild(authorListItem);
-      bookList.appendChild(pageNumListItem);
-      bookList.appendChild(readListItem);
-    }
-    
-    
-  }
-  return;
+	for (i = 0; i < arr.length; i++) {
+		console.log(arr[i].title);
+		// console.log(document.querySelectorAll('.titleListItem')[0].textContent);
+
+		// if item is already displayed on page don't run actions below
+
+		// make below section a standalone function to add book entry to web page
+
+	}
+	return;
 }
 
 bookIterator(libraryBooks, booksDiv);
-
-
 body.appendChild(booksDiv);
+
+function displayBookOnPage(obj){
+  let listItems = [];
+  const bookList = document.createElement('ul');
+  bookList.className = "bookList";
+  bookList.textContent = "";
+
+  let titleListItem = document.createElement('li');
+  titleListItem.className = "titleListItem";
+  titleListItem.textContent = `${obj.title}`;
+  listItems.push(titleListItem);
+
+  let authorListItem = document.createElement('li');
+  authorListItem.className = "authorListItem";
+  authorListItem.textContent = `${obj.author}`;
+  listItems.push(authorListItem);
+
+  let pageNumListItem = document.createElement('li');
+  pageNumListItem.className = "pageNumListItem";
+  pageNumListItem.textContent = `${obj.pageNum}`;
+  listItems.push(pageNumListItem);
+
+  let readListItem = document.createElement('li');
+  readListItem.className = "readListItem";
+  readListItem.textContent = `${obj.read}`;
+  listItems.push(readListItem);
+
+  booksDiv.appendChild(bookList);
+  bookList.appendChild(titleListItem);
+  bookList.appendChild(authorListItem);
+  bookList.appendChild(pageNumListItem);
+  bookList.appendChild(readListItem);
+}
+
+displayBookOnPage(cleanCode)
+
+
+
 
 // Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
 
 function formOpen() {
-  document.getElementById("new-book-entry").style.display = "block";
+	document.getElementById("new-book-entry").style.display = "block";
 }
 
 function formClose() {
-  document.getElementById("new-book-entry").style.display = "none";
+	document.getElementById("new-book-entry").style.display = "none";
 }
 
 // This section is for getting the data from the form and setting the variables for use in the object constructor.
 
-let title = document.getElementById('book-title')
-console.log(title)
+let title = document.getElementById('book-title');
+console.log(title);
 
-let author = document.getElementById('book-author')
-let pages = document.getElementById('book-pages')
+let author = document.getElementById('book-author');
+let pages = document.getElementById('book-pages');
 
-let read = document.getElementById('book-read')
+let read = document.getElementById('book-read');
 console.log(read);
 
 
 const submitButton = document.querySelector('#submit-btn');
-submitButton.addEventListener('click', (e)=>{
-  // e.stopPropagation();
-  // e.preventDefault();
-  console.log(e.target)
-  console.log(title.value);
-  console.log(author.value);
-  console.log(pages.value);
-  console.log(read.checked);
-  
-  // console.log(newEntry);
-  console.log(libraryBooks)
-
-  let newEntry = addLibraryBook(getInputValues(title.value, author.value, pages.value, read.checked));
-  // console.log(newEntry);
-  console.log(libraryBooks);
-  bookIterator(libraryBooks, booksDiv)
-  return newEntry;
- 
+submitButton.addEventListener('click', (e) => {
+	let newEntry = getInputValues(title.value, author.value, pages.value, read.checked);
+  addLibraryBook(newEntry)
+	return displayBookOnPage(newEntry)
+	
 });
-
-
-// console.log(title);
-// console.log(author);
-// console.log(pages);
-// console.log(read);
-
-const libraryRow = Array.from(document.getElementsByClassName('library-row'));
-// console.log(libraryRow);
-
-const libraryBox = document.createElement('div');
-// console.log(libraryBox)
-// let bookList = bookIterator(libraryBooks, libraryBox);
-// console.log(bookList)
-
-
-
 
 // Need to implement function that takes values from inputs and displays under library row title. 
 
 function getInputValues(title, author, pages, read) {
-  const val = new Books(title, author, pages, read)
-  return val;
+	const val = new Books(title, author, pages, read);
+	return val;
 }
 
-// let testBook = getInputValues('Mark', "John", 612, 1);
-// console.log(testBook)
-
-console.log(document.querySelectorAll('.titleListItem')[0].textContent)
-console.log(document.querySelectorAll('.titleListItem')[1].textContent)
-console.log(document.querySelectorAll('.titleListItem')[2].textContent)
-console.log(document.querySelectorAll('.titleListItem')[3].textContent)
+// console.log(document.querySelectorAll('.titleListItem')[0].textContent);
+// console.log(document.querySelectorAll('.titleListItem')[1].textContent);
+// console.log(document.querySelectorAll('.titleListItem')[2].textContent);
+// console.log(document.querySelectorAll('.titleListItem')[3].textContent);
