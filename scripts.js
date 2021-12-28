f4023b4b7bf1f0bcc3965410d29b7a932e548a57
xandre9-git <jmjames673@gmail.com>
2021-12-28 14:02:05 -0500

    Reformatted code and began work on 'localStorage' feature to save data so that it can be accessed after page refresh or later revisit.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ function Books(title, author, pageNum, read) { // object constructor
 	this.title = title;
 	this.author = author;
 	this.pageNum = pageNum;
-	this.read = read == 1 ? "Y" : "N"; // 1 means read, 0 means not read yet
-	this.info = function info(string) {
-		return string = `${this.title} by ${this.author}, ${this.pageNum} pages, read: ${this.read}`;
+	this.read = read == 1 ? "read" : "unread"; // 1 means read, 0 means not read yet
+	this.info = function info() {
+		return `${this.title} by ${this.author}. It has ${this.pageNum} pages and is currently ${this.read}.`;
 	};
 }
 
@@ -14,22 +14,24 @@ const cleanCode = new Books('Clean Code: A Handbook of Agile Software Craftsmans
 
 const eloquentJavaScript = new Books('Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming', 'Marijn Haverbeke', 472, 0);
 
-// console.log(rentalBook.info());
+console.log(rentalBook.info());
 // console.log(cleanCode.info());
 // console.log(eloquentJavaScript.info());
 
 // All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array.
 
-let libraryBooks = [];
+// JSON.parse(window.localStorage.getItem()) is used to get data set on line 114. It had to be parsed from string back to an object.
+let libraryBooks = JSON.parse(window.localStorage.getItem('book'));
+console.log(libraryBooks);
 
 function addLibraryBook(obj) {
 	libraryBooks.push(obj);
 }
 
 // test books
-addLibraryBook(eloquentJavaScript);
-addLibraryBook(cleanCode);
-addLibraryBook(rentalBook);
+// addLibraryBook(eloquentJavaScript);
+// addLibraryBook(cleanCode);
+// addLibraryBook(rentalBook);
 
 // console.log(libraryBooks);
 
@@ -39,59 +41,52 @@ const body = document.querySelector('body');
 const booksDiv = document.createElement('div');
 booksDiv.className = 'booksContainer';
 
-function bookIterator(arr, childNode) {
+function bookIterator(arr) {
 
-	for (i = 0; i < arr.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		console.log(arr[i].title);
-		// console.log(document.querySelectorAll('.titleListItem')[0].textContent);
-
-		// if item is already displayed on page don't run actions below
-
-		// make below section a standalone function to add book entry to web page
-
+		displayBookOnPage(arr[i]);
 	}
 	return;
 }
 
-bookIterator(libraryBooks, booksDiv);
+bookIterator(libraryBooks);
 body.appendChild(booksDiv);
 
-function displayBookOnPage(obj){
-  let listItems = [];
-  const bookList = document.createElement('ul');
-  bookList.className = "bookList";
-  bookList.textContent = "";
+function displayBookOnPage(obj) {
+	let listItems = [];
+	const bookList = document.createElement('ul');
+	bookList.className = "bookList";
+	bookList.textContent = "";
 
-  let titleListItem = document.createElement('li');
-  titleListItem.className = "titleListItem";
-  titleListItem.textContent = `${obj.title}`;
-  listItems.push(titleListItem);
+	let titleListItem = document.createElement('li');
+	titleListItem.className = "titleListItem";
+	titleListItem.textContent = `${obj.title}`;
+	listItems.push(titleListItem);
 
-  let authorListItem = document.createElement('li');
-  authorListItem.className = "authorListItem";
-  authorListItem.textContent = `${obj.author}`;
-  listItems.push(authorListItem);
+	let authorListItem = document.createElement('li');
+	authorListItem.className = "authorListItem";
+	authorListItem.textContent = `${obj.author}`;
+	listItems.push(authorListItem);
 
-  let pageNumListItem = document.createElement('li');
-  pageNumListItem.className = "pageNumListItem";
-  pageNumListItem.textContent = `${obj.pageNum}`;
-  listItems.push(pageNumListItem);
+	let pageNumListItem = document.createElement('li');
+	pageNumListItem.className = "pageNumListItem";
+	pageNumListItem.textContent = `${obj.pageNum}`;
+	listItems.push(pageNumListItem);
 
-  let readListItem = document.createElement('li');
-  readListItem.className = "readListItem";
-  readListItem.textContent = `${obj.read}`;
-  listItems.push(readListItem);
+	let readListItem = document.createElement('li');
+	readListItem.className = "readListItem";
+	readListItem.textContent = `${obj.read}`;
+	listItems.push(readListItem);
 
-  booksDiv.appendChild(bookList);
-  bookList.appendChild(titleListItem);
-  bookList.appendChild(authorListItem);
-  bookList.appendChild(pageNumListItem);
-  bookList.appendChild(readListItem);
+	booksDiv.appendChild(bookList);
+	bookList.appendChild(titleListItem);
+	bookList.appendChild(authorListItem);
+	bookList.appendChild(pageNumListItem);
+	bookList.appendChild(readListItem);
 }
 
-displayBookOnPage(cleanCode)
-
-
+// displayBookOnPage(cleanCode)
 
 
 // Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
@@ -107,21 +102,18 @@ function formClose() {
 // This section is for getting the data from the form and setting the variables for use in the object constructor.
 
 let title = document.getElementById('book-title');
-console.log(title);
-
 let author = document.getElementById('book-author');
 let pages = document.getElementById('book-pages');
-
 let read = document.getElementById('book-read');
-console.log(read);
-
 
 const submitButton = document.querySelector('#submit-btn');
-submitButton.addEventListener('click', (e) => {
+submitButton.addEventListener('click', () => {
 	let newEntry = getInputValues(title.value, author.value, pages.value, read.checked);
-  addLibraryBook(newEntry)
-	return displayBookOnPage(newEntry)
-	
+	addLibraryBook(newEntry);
+  // localStorage.setItem(key, value) needs to occur here in order to save new book entry into libraryBook array permanently
+  // localStorage can only store strings, objects/arrays will need to be converted using JSON.stringify() before passing into above function
+  window.localStorage.setItem('book', JSON.stringify(libraryBooks))
+	return displayBookOnPage(newEntry);
 });
 
 // Need to implement function that takes values from inputs and displays under library row title. 
@@ -130,8 +122,3 @@ function getInputValues(title, author, pages, read) {
 	const val = new Books(title, author, pages, read);
 	return val;
 }
-
-// console.log(document.querySelectorAll('.titleListItem')[0].textContent);
-// console.log(document.querySelectorAll('.titleListItem')[1].textContent);
-// console.log(document.querySelectorAll('.titleListItem')[2].textContent);
-// console.log(document.querySelectorAll('.titleListItem')[3].textContent);
